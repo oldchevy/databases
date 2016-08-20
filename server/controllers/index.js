c4e12aacea451b8d7270f34f53c07fc1f9bcc9f6
oldchevy <nick.olszowy@gmail.com>
2016-08-19 17:32:31 -0700

    Made schema, made GET query, added client
    -Working on linking client and server, flow of requests

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -2,7 +2,13 @@ var models = require('../models');
 
 module.exports = {
   messages: {
-    get: function (req, res) {}, // a function which handles a get request for all messages
+    get: function (req, res) {
+      models.messages.get(function(body) {
+        console.log('body: ', body);
+        res.write(body);
+        res.end();
+      });
+    }, // a function which handles a get request for all messages
     post: function (req, res) {} // a function which handles posting a message to the database
   },
 
--- server/models/index.js
+++ server/models/index.js
@@ -2,8 +2,22 @@ var db = require('../db');
 
 module.exports = {
   messages: {
-    get: function () {}, // a function which produces all the messages
-    post: function () {} // a function which can be used to insert a message into the database
+    get: function (callback) {
+      db.dbConnection.connect();
+
+      var getQuery = [
+        'SELECT U.NAME, M.ID, M.BODY, R.NAME, M.TIMESENT FROM MESSAGES M INNER JOIN',
+        'USERS U ON (M.USER_ID = U.ID) INNER JOIN',
+        'ROOMS R ON (M.ROOMS_ID = R.ID);'
+      ].join('');
+      db.dbConnection.query(getQuery, function(err, body) {
+        callback(body);
+      });
+    }, // a function which produces all the messages
+    post: function (messageObj) {
+      db.connect();
+
+    } // a function which can be used to insert a message into the database
   },
 
   users: {
